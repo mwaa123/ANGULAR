be07178e4d7a74e33f9a10fc1d112f3e5e7cba30
ruth <ruthwanjiramugo@gmail.com>
2020-05-19 21:55:23 +0300

    add the conflicted lost files

---
+++ Gallery/pic/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render,redirect
+from django.http  import HttpResponse
+from django.http  import HttpResponse,Http404
+
+# Create your views here.
+
+def welcome(request):
+
+    return render(request,'all-fold/welcome.html')
+
+
